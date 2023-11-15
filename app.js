6441b76ccfd7a52875653db2103cb2a064e50d40
LeonelMatias199 <leonel.matias01@gmail.com>
2023-11-15 00:58:13 -0300

    Agregado Nodemon

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const port = process.env.PORT||3000
+
+app.listen(port, () => { console.log('Servidor corriendo en http://localhost:${port}')})
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.listen(3000, () =>
-    console.log('Servidor corriendo'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
